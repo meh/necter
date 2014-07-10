2d8ad9575433f5f69fcc5e294fc7705cb2385b9e
meh <meh@schizofreni.co>
2014-07-10 22:44:16 +0200

    manager/technologies: add #[] search by index

--- lib/necter/manager/technologies.rb
+++ lib/necter/manager/technologies.rb
@@ -27,7 +27,11 @@ module Necter
 		end
 
 		def [](value)
-			by_path(value) || by_name(value) || by_type(value)
+			if Integer === value
+				to_a[value]
+			else
+				by_path(value) || by_name(value) || by_type(value)
+			end
 		end
 
 		def by_path(path)
