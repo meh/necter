befea1af3243ecfbd946bd9633ff229821540ea8
meh <meh@schizofreni.co>
2014-07-11 00:30:28 +0200

    network/ip: be explicit about the variant

--- lib/necter/network/ip.rb
+++ lib/necter/network/ip.rb
@@ -80,7 +80,7 @@ module Necter
 
 			def save!
 				@network.send! :SetProperty, "IPv#{@version || 4}.Configuration",
-					@hash["IPv#{@version || 4}.Configuration"]
+					DBus.variant("a{sv}", @hash["IPv#{@version || 4}.Configuration"])
 			end
 
 			def method=(value)
